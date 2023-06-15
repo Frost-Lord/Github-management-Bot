9ace2f1ce74e89c8c3308261489f0bac1012c8fa
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2023-06-15 13:38:48 +1000

    Create pull.js

---
+++ src/slash/pull/pull.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+const { GenRepo, UpdateRepo } = require("../../utils/api");
+const RepoSchema = require("../../../Database/Schema/repo.js")
+const client = global.client;
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName("pull")
+        .setDescription("Manage Pull Requests")
+        .addStringOption((option) =>
+            option
+                .setName("type")
+                .setDescription("Status")
+                .setRequired(true)
+                .addChoices(
+                    { name: "List", value: "List" },
+                    { name: "Create", value: "Create" },
+                    { name: "Delete", value: "Delete" },
+                    { name: "Update", value: "Update" },
+                    { name: "Merge", value: "Merge" },
+                    { name: "Comments", value: "Comments" }
+                )
+        ),
+    permissions: ["ManageGuild"],
+    name: "List",
+
+    async execute(interaction) {
+        let subCommand = interaction.options.getString("type");
+        await interaction.deferReply();
+
+        const Scema = await RepoSchema.findOne({ id: interaction.guild.id });
+        if (!Scema) return await GenRepo(interaction.guild.id).then(() => {
+            interaction.editReply({ content: "Please wait for 5 seconds and try again."})
+        })
+
+        const embed = new Discord.EmbedBuilder()
+        .setColor(0x90EE90)
+        .setTitle("**Pull Request:**")
+        .setTimestamp()
+
+        if (subCommand == "Create") {
+        } else if (subCommand == "Delete") {
+        } else if (subCommand == "Update") {
+        } else if (subCommand == "Merge") {
+        } else if (subCommand == "Comments") {
+        }
+
+    },
+};
